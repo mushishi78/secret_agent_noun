95ab3d0882f7ea2c18867d35e21d2b11b18dde1d
Max White <mushishi78@gmail.com>
2015-01-10 18:12:25 +0000

    Convert tabs to spaces

--- spec/secret_agent_noun_spec.rb
+++ spec/secret_agent_noun_spec.rb
@@ -1,43 +1,43 @@
 require 'secret_agent_noun'
 
 describe SecretAgentNoun do
-	subject { described_class }
+  subject { described_class }
 
-	describe '.agent_noun' do
-		let(:result) { subject.agent_noun(input) }
+  describe '.agent_noun' do
+    let(:result) { subject.agent_noun(input) }
 
-		context 'with a lowercase string' do
-			let(:input) { 'build' }
-			it { expect(result).to eq('builder') }
-		end
+    context 'with a lowercase string' do
+      let(:input) { 'build' }
+      it { expect(result).to eq('builder') }
+    end
 
-		context 'with a mixed case string' do
-			let(:input) { 'CiVilIze' }
-			it { expect(result).to eq('civilizer') }
-		end
+    context 'with a mixed case string' do
+      let(:input) { 'CiVilIze' }
+      it { expect(result).to eq('civilizer') }
+    end
 
-		context 'with a symbol' do
-			let(:input) { :massage }
-			it { expect(result).to eq('masseuse') }
-		end
-	end
+    context 'with a symbol' do
+      let(:input) { :massage }
+      it { expect(result).to eq('masseuse') }
+    end
+  end
 
-	describe '.verb' do
-		let(:result) { subject.verb(input) }
+  describe '.verb' do
+    let(:result) { subject.verb(input) }
 
-		context 'with a lowercase string' do
-			let(:input) { 'caller' }
-			it { expect(result).to eq('call') }
-		end
+    context 'with a lowercase string' do
+      let(:input) { 'caller' }
+      it { expect(result).to eq('call') }
+    end
 
-		context 'with a mixed case string' do
-			let(:input) { 'CreAtOr' }
-			it { expect(result).to eq('create') }
-		end
+    context 'with a mixed case string' do
+      let(:input) { 'CreAtOr' }
+      it { expect(result).to eq('create') }
+    end
 
-		context 'with a symbol' do
-			let(:input) { :organizer }
-			it { expect(result).to eq('organize') }
-		end
-	end
+    context 'with a symbol' do
+      let(:input) { :organizer }
+      it { expect(result).to eq('organize') }
+    end
+  end
 end
