ce1a59fdeb82bc89ec1fa40db430d353e1f1670c
Max White <mushishi78@gmail.com>
2015-01-10 07:53:53 +0000

    Remove tabs

--- lib/secret_agent_noun.rb
+++ lib/secret_agent_noun.rb
@@ -1,31 +1,31 @@
 require 'yaml'
 
 module SecretAgentNoun
-	class << self
-		def agent_noun(verb)
-			lookup[normalize(verb)]
-		end
+  class << self
+    def agent_noun(verb)
+      lookup[normalize(verb)]
+    end
 
-		def verb(agent_noun)
-			inverse_lookup[normalize(agent_noun)]
-		end
+    def verb(agent_noun)
+      inverse_lookup[normalize(agent_noun)]
+    end
 
-		private
+    private
 
-		def yaml_file
-			File.expand_path('../../db/verbs_to_agent_nouns.yml', __FILE__)
-		end
+    def yaml_file
+      File.expand_path('../../db/verbs_to_agent_nouns.yml', __FILE__)
+    end
 
-		def lookup
-			@lookup ||= YAML.load_file(yaml_file)
-		end
+    def lookup
+      @lookup ||= YAML.load_file(yaml_file)
+    end
 
-		def inverse_lookup
-			@inverse_lookup ||= yaml_file.invert
-		end
+    def inverse_lookup
+      @inverse_lookup ||= yaml_file.invert
+    end
 
-		def normalize(input)
-			input.to_s.downcase
-		end
-	end
+    def normalize(input)
+      input.to_s.downcase
+    end
+  end
 end
